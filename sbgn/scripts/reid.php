abb0c8e7ed05ccefefcbc2cf38aec45c4eb93566
Martin Scharm <martin@binfalse.de>
2015-03-12 11:57:51 +0100

    fixed id fixer: now ids are deleted

--- sbgn/scripts/reid.php
+++ sbgn/scripts/reid.php
@@ -1,18 +1,18 @@
 <?php
 
-$lines = explode ("\n", file_get_contents ("tmp4.gml"));
+$lines = explode ("\n", file_get_contents ("../process-description.gml"));
 
 $out = "";
 foreach ($lines as $l)
 {
 	if (strpos ($l, "ID \"glyph"))
-		$l = preg_replace ("/glyph/", "glyph" . rand (), $l);
+		continue;//$l = preg_replace ("/glyph/", "glyph" . rand (), $l);
 	if (strpos ($l, "ID \"arc"))
-		$l = preg_replace ("/arc/", "arc" . rand (), $l);
+		continue;//$l = preg_replace ("/arc/", "arc" . rand (), $l);
 	$out .= $l."\n";
 }
 
-file_put_contents ("tmp5.gml", $out);
+file_put_contents ("process-description.gml-fixed-ids.gml", $out);
 
 
 ?>
